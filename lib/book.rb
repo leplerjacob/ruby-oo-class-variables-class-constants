260f780f1d790c14462c1acf13d1f983813e9697
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-16 14:44:37 -0500

    commit tree 1e332f0c052e787dbfded7771b1f7c7c01ccfa92

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,18 @@
+pry = require 'pry'
+
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << @genre
   end
 
   def turn_page
